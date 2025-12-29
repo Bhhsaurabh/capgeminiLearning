af93550a193246e4df480d0f5bf163a77b5f4b48
Saurabh Kumar <bhhsaurabh@Saurabhs-MacBook-Air.local>
2025-12-29 12:09:01 +0530

    classes and objects

---
+++ Phase2/src/Classes/FaceBook.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class FaceBook {
+	int id;
+	String username;
+	String pass;
+	
+	public FaceBook(int id , String username , String pass) {
+		this.id = id;
+		this.username = username;
+		this.pass = pass;
+	}
+}
---
+++ Phase2/src/Classes/FacebookApp.java
@@ -0,0 +1,10 @@
+package Classes;
+
+public class FacebookApp {
+	public static void main(String[] args) {
+		FaceBook fb = new FaceBook(1,"bhhsaurabh","bhhsaurabh@gmail.com");
+		System.out.println(fb.id);
+		System.out.println(fb.username);
+		System.out.println(fb.pass);
+	}
+}
